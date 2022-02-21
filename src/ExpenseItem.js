595f2d9fe1a202739dc742ad35165b697720a6c4
Muhammad Bilal Nadeem <bilalofficialmaial123@gmail.com>
2022-02-21 20:37:02 +0000

    mod3: Writing More Complex JSX code in Expense.js

--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,13 +1,11 @@
 function ExpenseItem() {
   return (
     <div>
-      <h1>Expense Item</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa corrupti
-        ab numquam consequatur dolore iste laboriosam quibusdam maiores est,
-        inventore repellendus quasi sequi minus laudantium. Quo architecto
-        dolore ea, pariatur similique eos expedita impedit.
-      </p>
+      <div>March 28th 2021</div>
+      <div>
+        <h1>Car Insurance</h1>
+        <div>$294.67</div>
+      </div>
     </div>
   );
 }
