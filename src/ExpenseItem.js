e639af67989b3ccda0e8ce20b49770ad3f527ba5
Muhammad Bilal Nadeem <bilalofficialmaial123@gmail.com>
2022-02-21 20:31:42 +0000

    mod3: Created First React Component

---
+++ src/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h1>Expense Item</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa corrupti
+        ab numquam consequatur dolore iste laboriosam quibusdam maiores est,
+        inventore repellendus quasi sequi minus laudantium. Quo architecto
+        dolore ea, pariatur similique eos expedita impedit.
+      </p>
+    </div>
+  );
+}
+export default ExpenseItem;
