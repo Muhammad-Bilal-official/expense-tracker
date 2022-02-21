86e2e607aadb0679a825a8a5f467b28c675ea55c
Muhammad Bilal Nadeem <bilalofficialmaial123@gmail.com>
2022-02-21 20:46:48 +0000

    mod3: Adding basic css styling

--- src/ExpenseItem.js
+++ src/ExpenseItem.js
@@ -1,10 +1,11 @@
+import './ExpenseItem.css';
 function ExpenseItem() {
   return (
-    <div>
-      <div>March 28th 2021</div>
-      <div>
+    <div className='expense-item'>
+      <div >March 28th 2021</div>
+      <div className='expense-item__description'>
         <h1>Car Insurance</h1>
-        <div>$294.67</div>
+        <div className='expense-item__price'>$294.67</div>
       </div>
     </div>
   );
