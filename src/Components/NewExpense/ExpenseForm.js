59398c7d08c15d34a2480b878790e1849afe75fc
Muhammad Bilal Nadeem <bilalofficialmaial123@gmail.com>
2022-02-22 05:04:54 +0000

    mod4: Listening to user Input and printing it on console for watching the results

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -1,16 +1,25 @@
 import React from "react";
 import "./ExpenseForm.css";
 export default function ExpenseForm(props) {
+  const titleChangeHandler = (event) =>{
+    console.log(event.target.value);
+  }
+  const amountChangeHandler = (event) =>{
+    console.log(event.target.value);
+  }
+  const dateChangeHandler = (event) =>{
+    console.log(event.target.value);
+  }
   return (
     <form className='new-expense'>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label htmlFor="title">Title</label>
-          <input type="text" name="title" id="title" />
+          <input type="text" name="title" id="title"  onChange={titleChangeHandler}/>
         </div>
         <div className="new-expense__control">
           <label htmlFor="amount">Amount</label>
-          <input type="number" min="0.1" step="0.1" name="amount" id="amount" />
+          <input type="number" min="0.1" step="0.1" name="amount" id="amount" onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label htmlFor="date">Date</label>
@@ -20,6 +29,7 @@ export default function ExpenseForm(props) {
             id="date"
             min="2019-01-01"
             max="2022-12-31"
+            onChange={dateChangeHandler}
           />
         </div>
       </div>
