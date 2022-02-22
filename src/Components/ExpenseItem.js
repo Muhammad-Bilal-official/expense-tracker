9dba9fa1ea850b18ac18795eb09b26c07e1f7be8
Muhammad Bilal Nadeem <bilalofficialmaial123@gmail.com>
2022-02-22 03:26:49 +0000

    mod4: Working with State

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,16 +1,18 @@
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
+import React,{useState} from "react";
 const ExpenseItem = (props) => {
+  const [title,setTitle] =  useState(props.title);
   const clickHandler = () =>{
-    alert("Change the Title");
+    setTitle("Title Changed");
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       {/* // To convert Date into String otherwise react gives an error. */}
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
         <button onClick={clickHandler}>Change The Title</button>
       </div>
