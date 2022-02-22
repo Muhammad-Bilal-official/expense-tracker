b67d190adb8782a6d95ec2a7d802cb2945c5d6ee
Muhammad Bilal Nadeem <bilalofficialmaial123@gmail.com>
2022-02-22 03:12:56 +0000

    mod4: Listening to Events and and Working with Event Handler-added simple button and click handler on it to check the working only

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -2,6 +2,9 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 const ExpenseItem = (props) => {
+  const clickHandler = () =>{
+    alert("Change the Title");
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
@@ -9,6 +12,7 @@ const ExpenseItem = (props) => {
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change The Title</button>
       </div>
     </Card>
   );
